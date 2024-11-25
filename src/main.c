a36083ff9ea61dfa6c5e6403014fe28b08e8a320
Zffu <radi0o0@outlook.com>
2024-11-25 21:04:01 +0100

    feat: added basic argument parsing

--- src/main.c
+++ src/main.c
@@ -2,8 +2,30 @@
  * The main file of Lightstrike.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "./logging.c"
+
 int main(int argc, char* argv[]) {
 	if(argc < 2) {
-		printf("Error: Usage must
+		printf("%sError: lightstrike \"<command>\" [arguments]%s\n", TEXT_HRED, RESET);
+		return -1;
+	}
+
+	char* buff = malloc(512);
+	buff[0] = '\0';
+	
+	int adapt = 0;
+
+	for(int i = 1; i < argc; ++i) {
+		if(strcmp(argv[i], "-adapt") == 0) {
+			adapt = 1;
+		}
+		else {
+			if(i > 1) strcat(buff, " ");
+			strcat(buff, argv[i]);
+		}
 	}
 }
